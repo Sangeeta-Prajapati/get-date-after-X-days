cd6f156fe9f462d23a712ce3d2c88204fb044227
Sangeeta <sanjuother107@gmail.com>
2023-11-06 16:18:33 +0000

    final soln date after X days

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const addDays = require("date-fns/addDays");
 const getDateAfterGivenDate = (days) => {
-  const newDate = addDays(new Date(2020, 07, 22), day);
+  const newDate = addDays(new Date(2020, 7, 22), days);
   return `${newDate.getDate()}-${
     newDate.getMonth() + 1
-  } - ${newDate.getFullYear()}`;
+  }-${newDate.getFullYear()}`;
 };
 module.exports = getDateAfterGivenDate;
